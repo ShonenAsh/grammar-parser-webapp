65465ccde4c7dad723f58c2752146a72f0c02022
Ashish Magadum <shonenash@gmail.com>
2026-03-03 23:26:31 -0500

    pos tagger tested

---
+++ biber/biber.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+
+
+int main() {
+    std::string s1 = "This is a sample text";
+    std::cout<<s1<<std::endl;
+    return 0;
+}
